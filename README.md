c923037fe04756f51a58657ec43319464dad64a7
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-13 13:39:46 -0700

    Add html form content and JS business and user logic
    Co-authored-by: Mark Tiapkov <mark.tiapkov@gmail.com>

---
+++ README.md
@@ -0,0 +1,30 @@
+# Triangle Tracker
+
+#### Triangle Tracker created on 8/13/2020
+
+### By _Vanessa Stewart_ & _Mark Tiapkov_
+
+## Description
+This is a Triangle Tracker created to demonstrate knowedlge of js, and if else statements specifically
+
+## Installation/Setup
+* Clone this repository
+* Run index.html
+* See product in your browser window
+
+## Bugs/Issues
+There are no known bugs at the time. 
+
+## Contact/Support
+For feedback/support you can reach Vanessa on Discord @Vanessa Stewart
+#3573 or Mark Tiapkov @Mark T#0558
+
+# Technologies Used
+* Js
+* Jquery
+* HTML5
+
+### License
+This site is licensed under the MIT license
+
+Copyright © 2020 **_Vanessa Stewart_** & **_Mark Tiapkov_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,46 @@
 // Business Logic
-  // Create functions for equilateral, isocseles, scalene, not a triangle.
+function equTriangle (side1, side2, side3) {
+  if (side1 === side2 && side1 ===side3){
+    return true;
+  };
+};
+
+function isoTriangle (side1, side2, side3) {
+  if (side1 === side2 && side1 !== side3){
+    return true;
+  };
+};
+
+function scalTriangle (side1, side2, side3) {
+  if (side1 !== side2 && side1 !== side3 && side2 !== side3){
+    return true;
+  };
+};
+
+function notATriangle (side1, side2, side3) {
+  if (side1+side2 <= side3 || side1+side3 <= side2 || side2+side3 <= side1){
+    return true;
+  };
+};
 
 
 // User Interface Logic
-  // Grab the inputs, store them as variables
-  // Create Branching
-    // Run those variables through my business logic functions to determine what we have
-  // Return answers in return div
+  $(document).ready(function () {
+    $("#form").submit(function (event) {
+      event.preventDefault();
+      const side1Input = parseInt($("#side1").val());
+      const side2Input = parseInt($("#side2").val());
+      const side3Input = parseInt($("#side3").val());
+
+      if (equTriangle(side1Input, side2Input, side3Input)) {
+        $("#result").text("You have an equilateral triangle!")
+      } else if (isoTriangle(side1Input, side2Input, side3Input)) {
+        $("#result").text("You have an isosceles triangle!")
+      } else if (scalTriangle(side1Input, side2Input, side3Input)) {
+        $("#result").text("You have a scalene triangle!")
+      } else {
+        $("#result").text("Sorry, you do not have a triangle!")
+      }
+    });
+  });
  
