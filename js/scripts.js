5cf33d4f3a1f0e3ef095782779ebaf6795ffbbbe
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-13 12:00:00 -0700

    Add initial file structure with action item comments
    Co-authored-by: Mark Tiapkov <mark.tiapkov@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Business Logic
+  // Create functions for equilateral, isocseles, scalene, not a triangle.
+
+
+// User Interface Logic
+  // Grab the inputs, store them as variables
+  // Create Branching
+    // Run those variables through my business logic functions to determine what we have
+  // Return answers in return div
+ 
